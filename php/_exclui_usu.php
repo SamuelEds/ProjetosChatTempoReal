606dea29a4229c12ecbefc78f9366d9f836dff3b
Samuel Edson <samueledshotmailgm@gmail.com>
2020-03-07 10:32:04 -0300

    Quando o usuário exclui a conta, suas mensagens também são excluídos

--- php/_exclui_usu.php
+++ php/_exclui_usu.php
@@ -5,8 +5,18 @@ include("conexao.php");
 
 
 $email = $_SESSION['email'];
-$sql = "DELETE FROM usuarios WHERE email = '$email'"; 
 
+$sql2 = "SELECT * FROM usuarios WHERE email = '$email'";
+$result2 = mysqli_query($con,$sql2);
+
+while ($row = mysqli_fetch_assoc($result2)) {
+	$nome = $row['nomeusuario'];
+}
+
+$sql3 = "DELETE FROM mensagens WHERE nome = '$nome'"; 
+$result3 = mysqli_query($con,$sql3);
+
+$sql = "DELETE FROM usuarios WHERE email = '$email'"; 
 $result = mysqli_query($con,$sql);
 
 if(mysqli_affected_rows($con)){
